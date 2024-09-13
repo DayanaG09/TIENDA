9ea28f0d0f29ce30debcc8a7a8d6c75ec2c4cdcf
Dayana Garzon <haybet_garzon@soy.sena.edu.co>
2024-09-13 09:29:09 -0500

    modificaciones de la conexion

--- modeloProductos.py
+++ modeloProductos.py
@@ -1,92 +1,96 @@
-class __init__(self):
-    self.nombreProducto=None
-    self.pkProducto=None
-    self.detalles=None
-    self.categoria=None
-    self.existenciaProducto=None
-    self.cantidadesVendidas=None
-    self.precioProducto=None
-    self.datos=[]
+from basededatos import crearConexion
+
+class Producto:
     
-##Datos
-def getDatos():
-    return self.datos
-def setDatos(datos):
-    self.datos= datos
-##nombre productos  
-def getDatos():
-    return self.datos
-def setNombreProducto(name):
-    self.nombreProducto= name 
-## LLave primaria o codigo identificador del producto
-def getPrimaryKey():
-    return self.pkProducto
-def setPrimaryKey(codigo):
-    self.pkProducto= codigo 
- ## Existencia del producto   
-def getExistencia():
-    return self.existenciaProducto
-def setExistencia(existencia):
-    self.existenciaProducto= existencia 
-##Cantidades vendidas
-def getCantidadesVendidas():
-    return self.cantidadesVendidas
-def setCantidadesVendidas(productosVendidos):
-    self.cantidadesVendidas= productosVendidos 
-##precio productos
-def getPrecio():
-    return self.precioProducto
-def setPrecio(precio):
-    self.precioProducto= precio 
-#detalles
-def getDetalles():
-    return self.detalles
-def setDetalles(detalles):
-    self.detalles= detalles
-##categoria
-def getCategory():
-    return self.categoria
-def setCategory(categoria):
-    self.categoria= categoria
+    def __init__(self):
+        self.nombreProducto=None
+        self.pkProducto=None
+        self.detalles=None
+        self.categoria=None
+        self.existenciaProducto=None
+        self.cantidadesVendidas=None
+        self.precioProducto=None
+        self.datos=[]
     
+        ##Datos
+        def getDatos(self):
+            return self.datos
+        def setDatos(self, datos):
+            self.datos= datos
+        ##nombre productos  
+        def getNombreProducto(self):
+            return self.nombreProducto
+        def setNombreProducto(self, name):
+            self.nombreProducto= name 
+        ## LLave primaria o codigo identificador del producto
+        def getPrimaryKey(self):
+            return self.pkProducto
+        def setPrimaryKey(self, codigo):
+            self.pkProducto= codigo 
+        ## Existencia del producto   
+        def getExistencia(self):
+            return self.existenciaProducto
+        def setExistencia(self, existencia):
+            self.existenciaProducto= existencia 
+        ##Cantidades vendidas
+        def getCantidadesVendidas(self):
+            return self.cantidadesVendidas
+        def setCantidadesVendidas(self, productosVendidos):
+            self.cantidadesVendidas= productosVendidos 
+        ##precio productos
+        def getPrecio(self):
+            return self.precioProducto
+        def setPrecio(self, precio):
+            self.precioProducto= precio 
+        #detalles
+        def getDetalles(self):
+            return self.detalles
+        def setDetalles(self, detalles):
+            self.detalles= detalles
+        ##categoria
+        def getCategory(self):
+            return self.categoria
+        def setCategory(self, categoria):
+            self.categoria= categoria
+            
   ###REGISTRAR PRODUCTOS
   #1. SE HACE CONEXION CON LA BASE DE DATOS.  
-    
+            
     def registrarProducto (name, codigo,existencia, productosVendidos, precio, detalles, categoria):
-        conexion1 = conexion.crearConexion()
-        cursor = conexion1.cursor()
-        cursor.execute(f"INSERT INTO productos (name, codigo, existencia, productosVendidos, precio) VALUES ('{name}','{codigo}','{existencia}','{detalles}','{categoria}','{productosVendidos}','{precio}')")
-        conexion1.commit()
-        print("Datos Guardados con exito")
+                conexion1 = crearConexion()
+                cursor = conexion1.cursor()
+                cursor.execute(f"INSERT INTO productos (name, codigo, existencia, productosVendidos, precio) VALUES ('{name}','{codigo}','{existencia}','{detalles}','{categoria}','{productosVendidos}','{precio}')")
+                conexion1.commit()
+                print("Datos Guardados con exito")
     
 ##CONSULTAR O BUSCAR PRODUCTOS
 ##2. DESPUES DE TENER YA LOS PRODUCTOS REGISTRADOS PROCEDEMOS A BUSCARLOS.
-    def consultarProductos(productos):
-        conexion1 = conexion.crearConexion()
-        cursor = conexion1.cursor()
-        cursor.execute(f"SELECT * FROM productos")
-        consulta = cursor.fetchall()
-        return consulta
+    def consultarProductos():
+            conexion1 = crearConexion()
+            cursor = conexion1.cursor()
+            cursor.execute(f"SELECT * FROM productos")
+            consulta = cursor.fetchall()
+            return consulta
     
 ###CONSULTA DE CATEGORIA
     def consultaCategoria(categoria):
-        conexion1 = conexion.crearConexion()
-        cursor = conexion1.cursor()
-        cursor.execute(f"SELECT * FROM productos WHERE categoria = '{categoria}'")
-        consulta = cursor.fetchall()
-        return consulta
+            conexion1 = conexion1.crearConexion()
+            cursor = conexion1.cursor()
+            cursor.execute(f"SELECT * FROM productos WHERE categoria = '{categoria}'")
+            consulta = cursor.fetchall()
+            return consulta
     
 ##GENERAR INFORME
-def generarInforme(productosVendidos):
-            with open(f'{productosVendidos}', 'w', encoding='utf8') as archivo:
+##def generarInforme(productosVendidos):
+           ## with open(f'{productosVendidos}', 'w', encoding='utf8') as archivo:
 
-                json.dumps(modelo.datos, archivo)
-            print("informe generado")
+                ##json.dumps(modelo.datos, archivo)
+            ##print("informe generado")
             
             
-modelo.datos= modelo.consultarProductos()
+Producto.datos= Producto.consultarProductos()
 print(modelo.datos)
-respuesta = modelo.consultaCategoria()
+respuesta = Producto.consultaCategoria()
 print(respuesta)
 nombre_a=input("Nombre del producto:")
 detalles_a= input("Detalles: ")
@@ -94,12 +98,10 @@ categoria_a= input("Categoria: ")
 precio_a=float("Precio: ")
 Out=int("Cantidades vendidas: ")
 
+Producto.name = nombre_a
+Producto.detalles = detalles_a
+Producto.categoria = categoria_a
+Producto.precio= precio_a
+Producto.cantidadesVendidas= Out
 
-
-modelo.name = nombre_a
-modelo.detalles = detalles_a
-modelo.categoria = categoria_a
-modelo.precio= precio_a
-modelo.cantidadesVendidas= Out
-
-modelo.registrarProducto (modelo.name, modelo.detalles, modelo.categoria, modelo.precio, modelo.cantidadesVendidas)
+Producto.registrarProducto (modelo.name, modelo.detalles, modelo.categoria, modelo.precio, modelo.cantidadesVendidas)
--- modeloUsuario.py
+++ modeloUsuario.py
@@ -1,56 +1,60 @@
-class __init__(self):
-    self.usuario=None
-    self.idUsuario=None
-    self.celular=None
-    self.cargo=None
-    self.data=[]
+from basededatos import crearConexion
+
+class Usuario:
+    def __init__ (self):
     
-def getDatos():
-    return self.data
-def setDatos(datas):
-    self.data= datas
-def getUsuario():
-    return self.usuario
-def setUsuario(nombre):
-    self.usuario=nombre
-def getIdusuario():
-    return self.idUsuario
-def setIdusuario(identificador):
-    self.idUsuario= identificador
-def getCelular():
-    return self.celular
-def setCelular(contacto):
-    self.celular= contacto
-def getCargo():
-    return self.cargo
-def setCargo(nivel):
-    self.cargo= nivel
+        self.usuario=None
+        self.idUsuario=None
+        self.celular=None
+        self.cargo=None
+        self.data=[]
+      
+        def getDatos(self):
+            return self.data
+        def setDatos(self,datas):
+            self.data= datas
+        def getUsuario(self):
+            return self.usuario
+        def setUsuario(self, nombre):
+            self.usuario=nombre
+        def getIdusuario(self):
+            return self.idUsuario
+        def setIdusuario(self, identificador):
+            self.idUsuario= identificador
+        def getCelular(self):
+            return self.celular
+        def setCelular(self, contacto):
+            self.celular= contacto
+        def getCargo(self):
+            return self.cargo
+        def setCargo(self, nivel):
+            self.cargo= nivel
 
 
-def registrarUsuario(nombre, identificador, contacto, nivel):
-        conexion1 = conexion.crearConexion()
+    def registrarUsuario(nombre, identificador, contacto, nivel):
+        conexion1 = crearConexion()
         cursor = conexion1.cursor()
         cursor.execute(f"INSERT INTO uduarios (nombre, identificador, contacto, nivel) VALUES ('{nombre}','{identificador}','{contacto}','{nivel}')")
         conexion1.commit()
         print("Datos Guardados con exito")  
         
-def consultarUsuario(registrarUsuario):
-        conexion1 = conexion.crearConexion()
+    def consultarUsuario():
+        conexion1 = crearConexion()
         cursor = conexion1.cursor()
         cursor.execute(f"SELECT * FROM usuario")
         consulta = cursor.fetchall()
         return consulta
 
-modelo.data=modelo.consultarUsuario()
+Usuario.data=Usuario.consultarUsuario()
 
 nombre1= input("Nombre Usuario: ")
 documento= input("Numero de Documento: ")
 numero=input("Numero de Celular: ")
 cargo= input("Cargo desempeñado en la empresa Administrador/Vendedor")
 
-modelo.nombre= nombre1
-modelo.identificador= documento
-modelo.contacto= numero
-modelo.nivel= cargo
+Usuario.nombre= nombre1
+Usuario.identificador= documento
+Usuario.contacto= numero
+Usuario.nivel= cargo
 
-modelo.registrarUsuario(modelo.nombre, modelo.identificador, modelo.contacto, modelo.nivel)
+Usuario.registrarUsuario(modelo.nombre, modelo.identificador, modelo.contacto, modelo.nivel)
