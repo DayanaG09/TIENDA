2ddfa9ab6ce42830d31c0aa05d9b7fa6117b1ba0
juancho060805@gmail.com <juancho060805@gmail.com>
2024-10-04 09:19:29 -0500

    actualizacion de controlador

--- index/CONTROLADOR/controlador.py
+++ index/CONTROLADOR/controlador.py
@@ -1,7 +1,15 @@
 class Controlador:
-    def __init__(self,objModelo):
-        self.objModelo=objModelo
+    def __init__(self,objModeloUsuario,objModeloProducto,objModeloCategoria):
+        self.objModeloUsuario=objModeloUsuario
+        self.objModeloProducto=objModeloProducto
+        self.objModeloCategoria=objModeloCategoria
         
-    def tomarDatosInicioSesion(self,listaDatos):
-        listaUsuario=listaDatos
-        #self.objModelo.
+    def validar_inicio_sesion(self,listaUsuario):
+        listaUsuarioDatos=listaUsuario
+        self.objModeloUsuario.set_usuario(listaUsuarioDatos)
+        validacion_consulta=self.objModeloUsuario.validarUsuario()
+        return validacion_consulta
+    
+    def consultar_usuarios(self):
+        consultaUsuario=self.objModeloUsuario.consultar_usuario()
+        return consultaUsuario
