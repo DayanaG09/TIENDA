c30c4f894101b9c1260c8a5b42c749825cc2ca30
juancho060805@gmail.com <juancho060805@gmail.com>
2024-10-04 16:56:00 -0500

    actualizacion controlador

--- index/CONTROLADOR/controlador.py
+++ index/CONTROLADOR/controlador.py
@@ -13,3 +13,30 @@ class Controlador:
     def consultar_usuarios(self):
         consultaUsuario=self.objModeloUsuario.consultar_usuario()
         return consultaUsuario
+
+    def crear_producto(self, lista_productos):
+        lista_productos_datos=lista_productos
+        self.objModeloProducto.set_producto(lista_productos_datos)    
+        validar_consulta=self.objModeloProducto.crear_producto()
+        return validar_consulta
+        
+    def modificar_producto(self,lista_productos):
+        lista_productos_datos=lista_productos
+        self.objModeloProducto.setProducto(lista_productos_datos)    
+        validar_consulta=self.objModeloProducto.modificar_producto()
+        return validar_consulta
+        
+    def eliminar_producto(self,lista_productos):
+        lista_productos_datos=lista_productos
+        self.objModeloProducto.set_producto(lista_productos_datos)    
+        validar_consulta=self.objModeloProducto.eliminar_producto()
+        return validar_consulta
+        
+    def consulta_productos(self):
+        consulta_producto=self.objModeloUsuario.consultar_producto()
+        return consulta_producto
+        
+    def consulta_categoria(self):
+        consulta_categoria=self.objModeloCategoria.consultar_categoria()
+        return consulta_categoria
+    
